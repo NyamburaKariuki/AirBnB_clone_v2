5108f537441a537b2f4d63c65f9ac78965057427
NyamburaKariuki <prettimercii@gmail.cm>
2024-04-16 15:06:12 +0300

    DBStorage - States and Cities

---
+++ models/engine/db_storage.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python3
+"""This is the file storage class for AirBnB"""
+import json
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from models.amenity import Amenity
+from models.base_model import Base, BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = {"Amenity": Amenity, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
+
+
+class DBStorage:
+    """This class serializes instances to a JSON file and
+    deserializes JSON file
+    """
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """Initializes new instances of DBStorage.
+        """
+        try:
+            user = os.environ.get('HBNB_MYSQL_USER')
+            password = os.environ.get('HBNB_MYSQL_PWD')
+            host = os.environ.get('HBNB_MYSQL_HOST')
+            db = os.environ.get('HBNB_MYSQL_DB')
+            env = os.environ.get('HBNB_ENV')
+            attributes = [user, password, host, db]
+            for attribute in attributes:
+                if attribute is None:
+                    print("Missing attributes env var")
+
+            conn_str = "mysql+mysqldb://{}:{}@{}/{}".format(
+                        user, password, host, db)
+            self.__engine = create_engine(conn_str, pool_pre_ping=True)
+
+            if env == 'test':
+                Base.metadata.drop_all(bind=self.__engine, checkfirst=True)
+        except Exception as E:
+            print("raised exception in init")
+            print(E)
+
+    def all(self, cls=None):
+        """query on the current database session (self.__session) all objects
+        epending of the class name
+        """
+        new_dict = {}
+        for clss in classes:
+            if cls is None or cls is classes[clss] or cls is clss:
+                objs = self.__session.query(classes[clss]).all()
+                for obj in objs:
+                    key = obj.__class__.__name__ + '.' + obj.id
+                    new_dict[key] = obj
+        return (new_dict)
+
+    def new(self, obj):
+        """adds the object to the current database session (self.__session)
+        """
+        if obj and self.__session:
+            self.__session.add(obj)
+
+    def save(self):
+        """commits all changes of the current database session
+        (self.__session).
+        """
+        if self.__session:
+            self.__session.commit()
+
+    def delete(self, obj=None):
+        """deletes obj if not none from the current database session
+        (__objects).
+        """
+        try:
+            self.__session.delete(obj)
+        except Exception:
+            pass
+
+    def reload(self):
+        """creates all tables in the database (feature of SQLAlchemy).
+        """
+        try:
+            Base.metadata.create_all(self.__engine)
+            session_factory = sessionmaker(bind=self.__engine,
+                                           expire_on_commit=False)
+            self.__session = scoped_session(session_factory)
+        except Exception as E:
+            print(E)
+
+    def close(self):
+        """removes our session"""
+        self.__session.remove()
--- models/place.py
+++ models/place.py
@@ -1,18 +1,96 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from os import environ
+from sqlalchemy import Column, Float, ForeignKey, Integer, String, Table
+from sqlalchemy.orm import relationship
+import models
+from models.amenity import Amenity
+from models.base_model import Base, BaseModel
+
+if environ.get('HBNB_TYPE_STORAGE') == 'db':
+    place_amenity = Table('place_amenity', Base.metadata,
+                          Column('place_id', String(60),
+                                 ForeignKey('places.id', ondelete='CASCADE'),
+                                 primary_key=True,
+                                 nullable=False),
+                          Column('amenity_id', String(60),
+                                 ForeignKey('amenities.id',
+                                            ondelete='CASCADE'),
+                                 primary_key=True,
+                                 nullable=False))
 
 
-class Place(BaseModel):
-    """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+class Place(BaseModel, Base):
+    """
+    Define a place
+
+    Attributes:
+        __tablename__ (str): Place MySQL table name
+    """
+
+    if environ.get('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "places"
+
+        city_id = Column(String(60), ForeignKey("cities.id",
+                         ondelete='CASCADE'), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id",
+                         ondelete='CASCADE'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
+        reviews = relationship('Review', backref='place',
+                               cascade='all, delete-orphan',
+                               passive_deletes=True)
+        amenities = relationship('Amenity', backref='place_amenities',
+                                 cascade='all, delete',
+                                 secondary=place_amenity,
+                                 viewonly=False,
+                                 passive_deletes=True)
+
+    else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
+
+    def __init__(self, *args, **kwargs):
+        """initializes Place"""
+        super().__init__(*args, **kwargs)
+
+    if environ.get('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def reviews(self):
+            """Returns the list of Review instances with place_id equals to
+            the current Place.id.
+            """
+            from models.review import Review
+            return [review for review in models.storage.all(Review).values()
+                    if review.place_id == self.id]
+
+        @property
+        def amenities(self):
+            """Returns the list of Amenity instances based on the attribute
+            """
+            return [amenity for amenity in models.storage.all(Amenity).values()
+                    if amenity.place_id == self.id]
+
+        @amenities.setter
+        def amenities(self, obj):
+            """Handles append method for adding an Amenity.id to the attribute
+            amenity_ids.
+            """
+            if not isinstance(obj, Amenity):
+                return
+            self.amenity_ids.append(obj.id)
